10df203f4b42b81f1d39b1b3979de4b3a802d58e
Jaime Pastrana García <jaipas01@ucm.es>
2021-04-03 13:33:51 +0200

    Update Builder.java
    Se cambia el createInstance para que lance una excepción de IllegalArgument cuando falle el createTheInstance.

--- src/simulator/factories/Builder.java
+++ src/simulator/factories/Builder.java
@@ -1,6 +1,7 @@
 	package simulator.factories;
 
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public abstract class Builder <T>  {
@@ -16,7 +17,7 @@ public abstract class Builder <T>  {
 	};
 	
 	public T createInstance(JSONObject info) {//Revisar 
-		JSONObject template = getBuilderInfo();
+		/*JSONObject template = getBuilderInfo();
 		//Try catch de Jsonexception 
 		if(!info.has("Type") || !info.has("data") ||info.getString("type") != this.type)//Si no hay claves  o type no corresponde al "contructor"
 		{
@@ -29,10 +30,15 @@ public abstract class Builder <T>  {
 				//return null;
 				throw new IllegalArgumentException("Datos erroneos en la creacion del objeto...");
 			}
+		}*/
+		if(type == null || type.equals(info.getString("type")))//Esto se parece más a lo exlicado en las transparencias... ns
+			return null;
+		try{
+			return (T)createTheInstance(info.getJSONObject("data"));//Se crea la instancia con data.Ya que hemos reconocido el tipo
 		}
-		/*if(type == null || type.equals(info.getString("type")))//Esto se parece más a lo exlicado en las transparencias... ns
-			return null;*/
-		return (T)createTheInstance(info.getJSONObject("data"));//Se crea la instancia con data.Ya que hemos reconocido el tipo
+		catch(JSONException e){
+			throw new IllegalArgumentException("Datos erroneos en la creacion del objeto...");
+		}
 	}
 	
 	public JSONObject getBuilderInfo() {//Construccion de una plantilla.
