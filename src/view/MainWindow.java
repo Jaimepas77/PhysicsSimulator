8cb15c854c4568184ec17fef6c78d69ddb01725a
Jaime Pastrana García <jaipas01@ucm.es>
2021-05-12 21:23:17 +0200

    Update MainWindow.java
    Se refactoriza para que esté todo más claro.

--- src/view/MainWindow.java
+++ src/view/MainWindow.java
@@ -9,46 +9,69 @@ import javax.swing.*;
 
 import simulator.control.Controller;
 
-public class MainWindow extends JFrame{
+public class MainWindow extends JFrame {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	// ...
+	private ControlPanel controlPanel;
+	private BodiesTable bodiesTable;
+	private Viewer viewer;
+	private StatusBar statusBar;
+	
+	private JPanel centerPanel;
+
 	Controller controller;
 	public MainWindow(Controller controller) {
 		super("Physics Simulator");
 		this.controller = controller;
 		initGUI();
 	}
+	
 	private void initGUI() {
+		//Layout
 		JPanel mainPanel = new JPanel(new BorderLayout());
 		setContentPane(mainPanel);
-		// TODO complete this method to build the GUI
-		// ..
 		
-		//Paneles
-		ControlPanel controlPanel = new ControlPanel(controller);
-		BodiesTable bodiesTable = new BodiesTable(controller);
-		Viewer viewer = new Viewer(controller);
-		StatusBar statusBar = new StatusBar(controller);
-		
-		JPanel centerPanel = new JPanel();
-		centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
-		
+		//Inicialización de paneles y componentes que se insertarán en el borderLayout
+		initComponents();
+
+		//Añadir en el border layout
 		this.add(controlPanel, BorderLayout.PAGE_START);//Arriba (toolbar: botones de control)
-		this.add(centerPanel, BorderLayout.CENTER);//En medio (simulacion)
+		this.add(centerPanel, BorderLayout.CENTER);//En medio (simulación)
 		this.add(statusBar, BorderLayout.PAGE_END);//Abajo (barra de info)
 		
-		//Componentes de en medio (simulacion)
-		centerPanel.add(bodiesTable);
-		centerPanel.add(viewer);
-		//manejar las dimensiones desde la ventana principal
+		//Controlar el cierre de la ventana (pedir mensaje de confirmación)
+		setCloseDialog();
+		
+		//Ajustar las dimensiones
 		bodiesTable.setPreferredSize(new Dimension(600, 300));
 		viewer.setPreferredSize(new Dimension(600, 400));
+		setMinimumSize(new Dimension(650, 500));
 		
-		//Controlar el cierre de la ventana (pedir mensaje de confirmacion)
+		pack();
+		setVisible(true);
+	}
+	
+	private void initComponents() {
+
+		controlPanel = new ControlPanel(controller);//Control
+		
+		//Componentes de en medio (simulación)
+		bodiesTable = new BodiesTable(controller);
+		viewer = new Viewer(controller);
+		
+		centerPanel = new JPanel();//Simulación
+		centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
+		centerPanel.add(bodiesTable);
+		centerPanel.add(viewer);
+		
+		statusBar = new StatusBar(controller);//Estado
+	}
+	
+	private void setCloseDialog() {
+
 		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		this.addWindowListener(new WindowListener() {
 
@@ -76,13 +99,5 @@ public class MainWindow extends JFrame{
 			public void windowOpened(WindowEvent arg0) {
 			}
 		});
-		
-		setMinimumSize(new Dimension(650, 500));
-		pack();
-		setVisible(true);
 	}
-	
-	// other private/protected methods
-	// ...
-	
 }
